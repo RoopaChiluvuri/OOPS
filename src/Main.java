abe442bdb1e7b5fae9ce3f1e7e130c7abe448de1
RoopaChiluvuri <roopasree2509@gmail.com>
2024-07-16 14:29:52 -0700

    reduced coupling

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ public class Main {
         var employee = new Employee();
 
         employee.setBaseSalary(50);
-        System.out.println(employee.getBaseSalary());
-
         employee.setHourlyRate(10);
-        System.out.println(employee.getHourlyRate());
 
         int wage = employee.calculateWage(10);
 
