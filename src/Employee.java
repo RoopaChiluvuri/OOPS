7332281f13add8d2d3d4d668c0716256b696579e
RoopaChiluvuri <roopasree2509@gmail.com>
2024-07-16 13:59:07 -0700

    added getters and setters

---
+++ src/Employee.java
@@ -0,0 +1,28 @@
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+
+    public int calculateWage(int extraHours){
+        return baseSalary + (extraHours * hourlyRate);
+    }
+
+    public void setBaseSalary(int baseSalary) throws IllegalAccessException {
+        if (baseSalary <= 0)
+            throw new IllegalAccessException("Salary cannot be 0 or less");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary(){
+        return baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) throws IllegalAccessException {
+        if(hourlyRate <= 0)
+            throw new IllegalAccessException("Hourly rate cannot be 0 or less");
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlyRate(){
+        return hourlyRate;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text.toUpperCase());
+    public static void main(String[] args) throws IllegalAccessException {
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
+        var employee = new Employee();
+
+        employee.setBaseSalary(50);
+        System.out.println(employee.getBaseSalary());
+
+        employee.setHourlyRate(10);
+        System.out.println(employee.getHourlyRate());
+
+        int wage = employee.calculateWage(10);
+
+        System.out.println(wage);
     }
 }
